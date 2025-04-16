1ccde1df4c64be802800678c35e25be4afc83214
Sierra Sun <sirerrasunsuly@gmail.com>
2025-04-16 09:24:00 +0800

    feat: add custom not-found page as client component

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function NotFound() {
+  return (
+    <div style={{ padding: '4rem', textAlign: 'center' }}>
+      <h1 style={{ fontSize: '2rem', marginBottom: '1rem' }}>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+} 
